fa2a7c9fe94b6b0b50c30d746f32bcbd7c9653ad
Konstantinos Petrogiannopoulos <konpetrogian@gmail.com>
2022-01-16 17:24:18 +0200

    some progress(maybe not)

--- MSGlang.h
+++ MSGlang.h
@@ -1,8 +1,37 @@
+#include <iostream>
+#include <string>
+#include <functional>
+#include <variant>
+#include <map>
 
 #define let auto
 #define object Object()
 #define none Value()
 
 class Object {
+<<<<<<< HEAD
+    private:
+
+    public:
+        
+};
+
+class Value {
+    private:
+        enum Type {INT, DOUBLE, STRING, BOOL, FUNCTION, OBJECT, _NULL} type;
+        std::variant<int, double, std::string, bool, std::function<Value(void)>, Object> value;
+
+    public:
+        inline Value(){
+            type = _NULL;
+        }
+
+        template <typename T>;
+        inline Value(T arg){
+            value = arg;
+        }
+};
+=======
     
-}
+};
+>>>>>>> f016a70a6d4ec5bd0105c42a77c3822d25858473
---
+++ MessageParsing.cpp
@@ -0,0 +1,2 @@
+#include "MSGlang.h"
+
