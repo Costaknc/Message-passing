5ce07097c3fa7d1c4fb25d93c8e82d314ee24c2a
Konstantinos Petrogiannopoulos <konpetrogian@gmail.com>
2022-01-16 23:37:54 +0200

    updated MSGlang.h

--- MSGlang.h
+++ MSGlang.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <string>
 #include <functional>
@@ -6,53 +8,58 @@
 
 #define let auto
 #define object Object()
+#define values alpha,
+#define lambda []() -> Value
 #define none Value()
 
 class Value;
 
 class Object {
-    private:
+private:
+	
+public:
+	std::unordered_map<std::string, Value> map;
 
-    public:
-        std::unordered_map<std::string, Value> map;
+	inline Object operator[](const Object& obj) {
+		// do some stuff here boi
+		return Object();
+	}
+} alpha;
 
-        inline Object operator[](const Object& obj){
-            //do some stuff n $hit
-            return Object();
-        }
-        
-}alpha;
 
-inline void expand(){
 
+inline void expand() {
+	//do not remove me :)
 }
 
 template <typename T, typename... Types>
-inline void expand(T arg0, Types... rest){
-    alpha.map[std::to_string(alpha.map.size())] = Value(arg0);
-    return expand(rest...);
+inline void expand(T arg0, Types... rest) {
+	std::cout << typeid(arg0).name() << std::endl;
+	alpha.map[std::to_string(alpha.map.size())] = Value(arg0);
+	return expand(rest...);
 }
 
 template <typename... Types>
-inline Object operator,(Object obj, Types... args){
-    alpha.map.clear();
-    expnd(args...);
-    return alpha;
+inline Object operator,(Object obj, Types... args) {
+	alpha.map.clear();
+	expand(args...);
+	return alpha;
 }
 
+// unused for now
 class Value {
-    private:
-        enum Type {INT, DOUBLE, STRING, BOOL, FUNCTION, OBJECT, _NULL} type;
-        std::variant<int, double, std::string, bool, std::function<Value(void)>, Object> value;
+private:
+	enum Type {INT, DOUBLE, STRING, BOOL, FUNCTION, OBJECT, _NULL} type; // might be usefull
+	std::variant<int, double, std::string, bool, std::function<Value(void)>, Object> value; // something like a union
 
-    public:
-        inline Value(){
-            type = _NULL;
-        }
+public:
+	inline Value() {
+		type = _NULL;
+	}
 
-        template <typename T>
-        inline Value(T arg){
-            value = typeid(arg).name();
-            type = value;
-        }
+	template <typename T>
+	inline Value(T arg) {
+		//assign correct type to 'type' variable ( maybe using typeid(arg).name() ) - might be usefull
+		value = arg;
+	}
 };
